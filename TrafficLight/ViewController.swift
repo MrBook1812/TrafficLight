94882f68524cb57c9af3947d05bf8b732f076ab9
Kirill Kniga <kiruha_95@icloud.com>
2023-02-05 15:33:45 +0300

    Work with srackView Constrains

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -26,15 +26,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.layer.cornerRadius = redLight.frame.width / 2
-        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
-        greenLight.layer.cornerRadius = greenLight.frame.width / 2
-        
         redLight.alpha = lightIsOff
         yellowLight.alpha = lightIsOff
         greenLight.alpha = lightIsOff
         
         startButton.layer.cornerRadius = 15
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
 
     @IBAction func startButtonTap(_ sender: UIButton) {
